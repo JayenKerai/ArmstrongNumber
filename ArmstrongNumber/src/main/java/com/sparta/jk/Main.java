580157fa94f6cd08f1b6733154f24901ff3782a9
JayenKerai <32294741+JayenKerai@users.noreply.github.com>
2020-02-04 19:40:40 +0000

    Update Main.java

--- ArmstrongNumber/src/main/java/com/sparta/jk/Main.java
+++ ArmstrongNumber/src/main/java/com/sparta/jk/Main.java
@@ -3,6 +3,6 @@ package com.sparta.jk;
 public class Main {
     public static void main(String[] args) {
         ArmstrongNumberFinder armstrongNumberFinder = new ArmstrongNumberFinder();
-        System.out.println(armstrongNumberFinder.isArmstrongNumber(153)); //print true or false depending on input for .isArmstrongNumber
+        System.out.println(armstrongNumberFinder.isArmstrongNumber(153)); //print true or false depending on input for .isArmstrongNumber()
     }
 }
